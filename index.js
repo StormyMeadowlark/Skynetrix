77ddef3217e8a417d4216a2f6c89c68637b26387
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-08-21 08:24:25 -0500

    Updates for heroku deployment

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ app.get("/", (req, res) => {
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    poolSize: 10,
   })
   .then(() => {
     console.log("Connected to MongoDB");
